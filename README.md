293d05a2e75062f3af62b4bd4fea2019e09a18e9
Dan Runt <dan.runt@gmail.com>
2016-09-15 09:31:58 -0400

    modified to use the bootstrap framework

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# P1" 
+## P1 Project
+
+The live link for this project is http:\\p1.danrunt.me
+
+The demo link is 
+
+Much of the CSS work I got using W3schools tutorial (Simply Me Theme) for the bootstrap framework.
--- index.php
+++ index.php
@@ -5,27 +5,54 @@
 	<title>Daniel Runt P1 - DWA15 Portfolio</title>
 	<meta charset="utf-8">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<link href='css/main.css' rel='stylesheet'>
+
+<!--	<link href='css/main.css' rel='stylesheet'>-->
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+	<style>
+	.bg-1 {
+		background-color:#474e5d;
+		color:#ffffff;
+	}
+	.bg-2 {
+		background-color:#ffffff;
+		color:#555555;
+	}
+	.container-fluid {
+		padding-top: 70px;
+		padding-bottom: 70px;
+	}
+	</style>
+
 </head>
 <body>
 
-	<div>
+	<div class="container-fluid bg-1 text-center">
 
-		<h1>Daniel Runt</h1>
+		<h1 class="margin-base-vertical">Daniel Runt</h1>
 
-		<img src='images/photo.jpg' alt='Dan Runt'>
+		<p><img class="img-circle" src='images/photo.jpg' alt='Dan Runt'></p>
+	</div>
 
-		<h3><a href="http:\\p2.danrunt.me\">Project 2 Title</a></h3>
-		<p>Github: <a href="http:\\github\drizzl\P2">[link]</a></p>
+	<div class="container-fluid bg-2 text container">
+		<div class="row">
+			<div class="col-md-4">
+				<a href="http:\\p2.danrunt.me\" class="btn btn-primary btn-lg" role="button">Project 2</a>
+				<p>Github: <a href="http:\\github.com\drizzl\P2">[link]</a></p>
+			</div>
 
-		<h3><a href="http:\\p3.danrunt.me\">Project 3 Title</a></h3>
-		<p>Github: <a href="http:\\p3.danrunt.me\P3">[link]</a></p>
+			<div class="col-md-4">
+				<a href="http:\\p3.danrunt.me\" class="btn btn-primary btn-lg" role="button">Project 3</a>
+				<p>Github: <a href="http:\\github.com\drizzl\P3">[link]</a></p>
+			</div>
 
-		<h3><a href="http:\\p4.danrunt.me\">Project 4 Title</a></h3>
-		<p>Github: <a href="http:\\p4.danrunt.me\P4">[link]</a></p>
-
+			<div class="col-md-4">
+				<a href="http:\\p4.danrunt.me\" class="btn btn-primary btn-lg" role="button">Project 4</a>
+				<p>Github: <a href="http:\\github.com\drizzl\P4">[link]</a></p>
+			</div>
+		</div>
 	</div>
 
 </body>
