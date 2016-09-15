5576015abb8346fb1677dd700d733fca2d8f805c
Dan Runt <dan.runt@gmail.com>
2016-09-14 21:27:11 -0400

    added CSS file and chagnes to index.php

--- index.php
+++ index.php
@@ -2,30 +2,29 @@
 <html>
 <head>
 
-	<title>Jackie Flores P1 - DWA15 Portfolio</title>
+	<title>Daniel Runt P1 - DWA15 Portfolio</title>
 	<meta charset="utf-8">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='css/main.css' rel='stylesheet'>
 </head>
 <body>
 
 	<div>
 
-		<h1>Jackie Flores</h1>
+		<h1>Daniel Runt</h1>
 
-		<img src='images/photo.jpg' alt='Doris Flores'>
+		<img src='images/photo.jpg' alt='Dan Runt'>
 
-		<h3>Project 2 Title</h3>
-		<p>View: [link]</p>
-		<p>Github: [link]</p>
+		<h3><a href="http:\\p2.danrunt.me\">Project 2 Title</a></h3>
+		<p>Github: <a href="http:\\github\drizzl\P2">[link]</a></p>
 
-		<h3>Project 3 Title</h3>
-		<p>View: [link]</p>
-		<p>Github: [link]</p>
+		<h3><a href="http:\\p3.danrunt.me\">Project 3 Title</a></h3>
+		<p>Github: <a href="http:\\p3.danrunt.me\P3">[link]</a></p>
 
-		<h3>Project 4 Title</h3>
-		<p>View: [link]</p>
-		<p>Github: [link]</p>
+		<h3><a href="http:\\p4.danrunt.me\">Project 4 Title</a></h3>
+		<p>Github: <a href="http:\\p4.danrunt.me\P4">[link]</a></p>
 
 	</div>
 
