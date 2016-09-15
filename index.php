029895fa851d6e076e1ef1a03f74ce8cb1e6e17e
Dan Runt <dan.runt@gmail.com>
2016-09-14 20:12:33 -0400

    Testing commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Jackie Flores P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+	<div>
+
+		<h1>Jackie Flores</h1>
+
+		<img src='images/photo.jpg' alt='Doris Flores'>
+
+		<h3>Project 2 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 3 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 4 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+	</div>
+
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Jackie Flores P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel='stylesheet' href='//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+	<link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Jackie Flores</h1>
+
+		<img src='images/photo.jpg' alt='Doris Flores'>
+
+		<h3>Project 2 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 3 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 4 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+	</div>
+
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
